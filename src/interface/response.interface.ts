7182a9319783a98feb432f945b06d428d71eb81a
Antonio Maranguape <antonio.maranguape@gmail.com>
2026-03-03 17:15:08 -0400

    feat: interface types

---
+++ src/interface/response.interface.ts
@@ -0,0 +1,7 @@
+import type { PaginationLinks, PaginationMeta } from "./pagination.interface";
+
+export interface PaginatedResponse<T> {
+  items: T[];
+  meta: PaginationMeta;
+  links: PaginationLinks;
+}
